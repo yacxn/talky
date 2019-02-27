73647076fb20fe87e8608b1c8c6936d80553bf45
yacine <yacinebenz3@gmail.com>
2019-02-27 16:58:43 +0000

    fixed socket import

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
-	 io.set('origins', '*:*');
+	io.set('origins', '*:*');
 usernames = [];
 
 server.listen(process.env.PORT || 3000);
@@ -13,8 +13,6 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-
-
 	socket.on('new user', function (data, callback) {
 		if (usernames.indexOf(data) != -1) {
 			callback(false);
@@ -25,23 +23,16 @@ io.sockets.on('connection', function (socket) {
 			updateUsernames();
 		}
 	});
-
-
 	function updateUsernames() {
 		io.sockets.emit('usernames', usernames);
 	}
-
-
 	socket.on('send message', function (data) {
 		io.sockets.emit('new message', { msg: data, user: socket.username });
 	});
-
-
 	socket.on('disconnect', function (data) {
 		if (!socket.username) {
 			return;
 		}
-
 		usernames.splice(usernames.indexOf(socket.username), 1);
 		updateUsernames();
 	});
